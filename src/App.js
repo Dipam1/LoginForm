872ac17d6241d5ba4e304deb4d3d4477139caa57
deepum <deepumpoudel@gmail.com>
2020-12-25 12:49:43 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './Components/Forms/LoginForm'
+
+function App() {
+  return (
+    <div className="form-div">
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
